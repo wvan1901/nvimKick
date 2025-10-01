0097e0ca1bffbfd61a2718542a8af34c10a74b73
wvan1901 <wvan1901@gmail.com>
2025-09-30 22:42:03 -0500

    bug: insert tab cursor moves

--- init.lua
+++ init.lua
@@ -850,6 +850,7 @@ require('lazy').setup({
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
+        ['<Tab>'] = {}, -- BUG: Without this when tabing while editing cursor moves randomly
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
